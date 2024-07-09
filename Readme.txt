671ffb768c46216dbb0c3cb2ac34b42a3dcf58eb
Areej <areejfatimasheikh25@gmail.com>
2024-07-09 21:06:27 +0530

    Your commit message

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+ https://areejsheikh.github.io/Triply/
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const header=document.querySelector("header");
+let flag = 0;
 
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky",window.scrollY>60)
@@ -7,6 +8,15 @@ window.addEventListener("scroll", function(){
 let menu=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
 menu.onclick = ()=> {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+    if(flag==0){
+
+        menu.classList.toggle('bx-x');
+        navbar.style.left="50%";
+        flag=1
+    }else{
+        menu.classList.toggle('bx-x');
+
+        navbar.style.left="100%";
+        flag=0
+    }
 };
